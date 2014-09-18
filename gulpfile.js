39eaac918faca7c0a2f05944e14a9434bef5bfa6
Chao-Ju Huang <chaoju.huang@gmail.com>
2014-09-18 12:22:38 +0800

    setup gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
@@ -10,7 +9,8 @@ var gulp = require('gulp'),
     notify = require('gulp-notify'),
     cache = require('gulp-cache'),
     livereload = require('gulp-livereload'),
-    del = require('del');
+    del = require('del'),
+    react = require('gulp-react');
 
 
 gulp.task('styles', function() {
@@ -22,4 +22,54 @@ gulp.task('styles', function() {
     .pipe(minifycss())
     .pipe(gulp.dest('dist/assets/css'))
     .pipe(notify({ message: 'Styles task complete' }));
+});
+
+
+gulp.task('scripts', function() {
+  return gulp.src('src/scripts/**/*.jsx')
+    .pipe(react())
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+
+gulp.task('images', function() {
+  return gulp.src('src/images/**/*')
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('dist/assets/img'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+
+gulp.task('clean', function(cb) {
+  del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], cb)
+});
+
+
+gulp.task('default', ['clean'], function() {
+  gulp.start('styles', 'scripts', 'images');
+});
+
+
+// Watch
+gulp.task('watch', function() {
+ 
+  // Watch .scss files
+  gulp.watch('src/styles/**/*.scss', ['styles']);
+ 
+  // Watch .js files
+  gulp.watch('src/scripts/**/*.jsx', ['scripts']);
+ 
+  // Watch image files
+  gulp.watch('src/images/**/*', ['images']);
+ 
+  // Create LiveReload server
+  livereload.listen(8000);
+ 
+  // Watch any files in dist/, reload on change
+  gulp.watch(['dist/**']).on('change', livereload.changed);
+
 });
